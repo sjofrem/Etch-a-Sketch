5cb2f7cbf6d4025bbd564b8d884ac8f4e6c6997c
sjofrem <sebastian.jofrem@usm.cl>
2021-10-17 21:35:17 -0300

    add the clear function and a slider to customize the size of the grid

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
+const grid = document.getElementById("grid");
+const clear = document.getElementById("clear");
+const slider_value = document.getElementById("range");
+const slider_output = document.getElementById("output");
+const new_grid_button = document.getElementById("new_grid");
 
-function creat_grid(){
-    let grid = document.getElementById("grid");
-    for(let i=0;i<16*16;i++){
-        let item = document.createElement("div");
+let update = () => output.innerText = `${slider_value.value}x${slider_value.value}`;
+
+new_grid_button.addEventListener("click", function() {
+    new_grid(slider_value.value);
+});
+slider_value.addEventListener("input", update);
+clear.addEventListener("click",clear_grid);
+
+function creat_grid(value){
+    for(let i=0;i<value*value;i++){
+        const item = document.createElement("div");
+        item.addEventListener("mouseover", pen_effect);
         grid.appendChild(item);
     }
+    grid.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${value}, 1fr)`;
 }
-creat_grid();
+
+function pen_effect(e){
+    e.target.style.backgroundColor = "#2b2928";
+
+}
+
+function clear_grid(){
+    const items = document.querySelectorAll("#grid > *");
+    for(let i = 0; i<items.length;i++){
+        items[i].style.backgroundColor = "#f2f1ef";
+    }
+
+}
+function new_grid(value){
+    clear_grid();
+    while (grid.lastElementChild) {
+        grid.removeChild(grid.lastElementChild);
+    }
+    creat_grid(value);
+}
+
+
+
+creat_grid(slider_value.value);
+
