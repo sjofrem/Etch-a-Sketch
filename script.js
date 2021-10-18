9ba16d99c051966dc305fdaca510c96095b6ad31
sjofrem <sebastian.jofrem@usm.cl>
2021-10-18 02:36:47 -0300

    added a color picker, color button, eraser button, rainbow botton, gray scale button and media queries

--- script.js
+++ script.js
@@ -3,9 +3,36 @@ const clear = document.getElementById("clear");
 const slider_value = document.getElementById("range");
 const slider_output = document.getElementById("output");
 const new_grid_button = document.getElementById("new_grid");
+const colorPicker = document.getElementById("colorPicker");
+const color = document.getElementById("color");
+const eraser = document.getElementById("eraser");
+const rainbow = document.getElementById("rainbow");
+const grey = document.getElementById("grey");
+
+let pen_type = 1;
 
 let update = () => output.innerText = `${slider_value.value}x${slider_value.value}`;
 
+color.addEventListener("click", function() {
+    pen_type=1;
+    removeSelection();
+    color.classList.add("active");
+});
+eraser.addEventListener("click",function() {
+    pen_type=2;
+    removeSelection();
+    eraser.classList.add("active");
+});
+rainbow.addEventListener("click", function() {
+    pen_type=3;
+    removeSelection();
+    rainbow.classList.add("active");
+});
+grey.addEventListener("click", function() {
+    pen_type=4;
+    removeSelection();
+    grey.classList.add("active");
+});
 new_grid_button.addEventListener("click", function() {
     new_grid(slider_value.value);
 });
@@ -22,8 +49,38 @@ function creat_grid(value){
     grid.style.gridTemplateRows = `repeat(${value}, 1fr)`;
 }
 
+function removeSelection(){
+    const selection = document.querySelector(".active");
+    selection.classList.remove("active");
+}
+
 function pen_effect(e){
-    e.target.style.backgroundColor = "#2b2928";
+    if(pen_type == 1){
+        e.target.style.backgroundColor = `${colorPicker.value}`;
+    }
+    else if(pen_type == 2){
+        e.target.style.backgroundColor = "#f2f1ef";
+    }
+    else if(pen_type == 3){
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        e.target.style.backgroundColor = `#${randomColor}`;
+    }
+    else if(pen_type == 4){
+        if(this.style.backgroundColor.match(/rgba/)){
+            let currentColor = Number(this.style.backgroundColor.slice(-4,-1));
+            if(currentColor <= 0.9){
+                this.style.backgroundColor = `rgba(0, 0, 0, ${currentColor+0.1})`;
+            }
+        }
+        else if(this.style.backgroundColor == `rgb(0, 0, 0)`){
+            return;
+        }
+        else{
+            this.style.backgroundColor = `rgba(0, 0, 0, 0.1)`;
+        }
+        
+
+    }
 
 }
 
